4a54846ff3a26d7519fc6c297c56c7f655ca0e28
Karina Ferreira <96082465+Karina2510@users.noreply.github.com>
2022-08-05 15:35:02 -0300

    inclusao do projeto de banco digital

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Rosimar");
+
+        IConta cc = new ContaCorrente(cliente);
+        IConta poupanca = new ContaPoupanca(cliente);
+
+        cc.depositar(100);
+
+        cc.transferir(90, poupanca);
+        poupanca.sacar(15);
+        cc.sacar(2);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+
+    }
+}
